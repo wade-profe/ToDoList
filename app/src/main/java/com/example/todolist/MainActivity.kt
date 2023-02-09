5c23718c38ad6adee3c15294f53f125f44acdd33
wade-profe <wade.profe@gmail.com>
2023-02-09 20:58:44 +0100

    New changes

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.todolist
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import com.example.todolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding : ActivityMainBinding
+    lateinit var sharedPreferences: SharedPreferences
+    var toDoList : ArrayList<String> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+        setContentView(view)
+
+        mainBinding.editTextNewItem.setOnKeyListener(View.OnKeyListener{v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP && !mainBinding.editTextNewItem.text.equals("")) {
+                toDoList.add(mainBinding.editTextNewItem.text.toString())
+                // TODO: update adapter 
+                return@OnKeyListener true
+            }
+            false
+        })
+
+
+
     }
 }
